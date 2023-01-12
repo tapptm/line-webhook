55847482843c3154938d420803cd6082117132db
tapptm <phuwadet00123@gmail.com>
2023-01-12 16:19:22 +0700

    update index.js

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const express = require("express");
+const { WebhookClient } = require("dialogflow-fulfillment");
 const app = express();
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
 const port = process.env.PORT || 4050;
 
-const { WebhookClient } = require("dialogflow-fulfillment");
+app.use(express.json());
 
-app.use(morgan("dev"));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send("Server Is Working......")
-})
+app.get("/", (req, res) => {
+  res.send("Server Is Working......");
+});
 /**
-* on this route dialogflow send the webhook request
-* For the dialogflow we need POST Route.
-* */
-app.post('/webhook', (req, res) => {
+ * on this route dialogflow send the webhook request
+ * For the dialogflow we need POST Route.
+ * */
+app.post("/webhook", (req, res) => {
   // get agent from request
-  let agent = new WebhookClient({request: req, response: res})
+  let agent = new WebhookClient({ request: req, response: res });
   // create intentMap for handle intent
   let intentMap = new Map();
   // add intent map 2nd parameter pass function
-  intentMap.set('webhook-demo',handleWebHookIntent)
+  intentMap.set("webhook-demo", handleWebHookIntent);
   // now agent is handle request and pass intent map
-  agent.handleRequest(intentMap)
-})
-function handleWebHookIntent(agent){
-  agent.add("Hello I am Webhook demo How are you...")
+  agent.handleRequest(intentMap);
+});
+function handleWebHookIntent(agent) {
+  agent.add("Hello I am Webhook demo How are you...");
 }
 
 app.listen(port, () => {
