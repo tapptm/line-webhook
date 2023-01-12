2119dba7cc4fbf3597be882de17356b3d2ad64f3
tapptm <phuwadet00123@gmail.com>
2023-01-12 16:13:30 +0700

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4050;
 
 const { WebhookClient } = require("dialogflow-fulfillment");
 
