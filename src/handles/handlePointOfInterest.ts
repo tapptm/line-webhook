cda33543dbd405ea247d70988bb9961fe864dda3
tapptm <phuwadet00123@gmail.com>
2023-01-17 02:15:04 +0700

    [fix]: line response payloads

--- src/handles/handlePointOfInterest.ts
+++ src/handles/handlePointOfInterest.ts
@@ -148,63 +148,7 @@ async function getATMlocation(agent: {
                 uri: "http://maps.google.com/maps?z=12&t=m&q=loc:14.92958496+103.77651798",
               },
             ],
-          },
-          {
-            thumbnailImageUrl:
-              "https://admin.trinitytrip.com/uploads/community/6/poi/poi_cd8b791439946d1d49d83082c513eb19_20211123072951000000.jpg",
-            imageBackgroundColor: "#FFFFFF",
-            title: "ATM ธนาคารออมสิน หน้า 7-11",
-            text: "ATM ธนาคารออมสิน หน้า 7-11",
-            actions: [
-              {
-                type: "uri",
-                label: "เปิดแผนที่",
-                uri: "http://maps.google.com/maps?z=12&t=m&q=loc:14.94458201+103.78640238",
-              },
-            ],
-          },
-          {
-            thumbnailImageUrl:
-              "https://admin.trinitytrip.com/uploads/community/10/poi/poi_cd8b791439946d1d49d83082c513eb19_20211125043958000000.jpg",
-            imageBackgroundColor: "#FFFFFF",
-            title: "ATM ธนาคารกรุงไทย ปั๊ม ปตท.บจก.สินชัยกาญจน์ปิโตรเลียม",
-            text: "ATM ธนาคารกรุงไทย ปั๊ม ปตท.บจก.สินชัยกาญจน์ปิโตรเลียม",
-            actions: [
-              {
-                type: "uri",
-                label: "เปิดแผนที่",
-                uri: "http://maps.google.com/maps?z=12&t=m&q=loc:14.93870261+103.78914122",
-              },
-            ],
-          },
-          {
-            thumbnailImageUrl:
-              "https://admin.trinitytrip.com/uploads/community/1/poi/poi_f1aa1edabea867100c6f930ef7fc063d_20211007163337000000.jpg",
-            imageBackgroundColor: "#FFFFFF",
-            title: "ATM Krungthai Bank",
-            text: "ATM Krungthai Bank",
-            actions: [
-              {
-                type: "uri",
-                label: "เปิดแผนที่",
-                uri: "http://maps.google.com/maps?z=12&t=m&q=loc:14.94111+103.805011",
-              },
-            ],
-          },
-          {
-            thumbnailImageUrl:
-              "https://admin.trinitytrip.com/uploads/community/1/poi/poi_081d05881d24d2c0e2e6e83b500d231f_20211012034959000000.jpg",
-            imageBackgroundColor: "#FFFFFF",
-            title: "ATM Krungthai Bank",
-            text: "ATM Krungthai Bank",
-            actions: [
-              {
-                type: "uri",
-                label: "เปิดแผนที่",
-                uri: "http://maps.google.com/maps?z=12&t=m&q=loc:14.94111+103.805011",
-              },
-            ],
-          },
+          }
         ],
       },
     },
