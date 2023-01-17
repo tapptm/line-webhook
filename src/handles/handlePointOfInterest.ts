d4920d47a0a7b641d6418029e788874bb204e506
tapptm <phuwadet00123@gmail.com>
2023-01-17 14:40:57 +0700

    [fix]: line response payloads

--- src/handles/handlePointOfInterest.ts
+++ src/handles/handlePointOfInterest.ts
@@ -16,31 +16,117 @@ async function getlocation(agent: Agent) {
    * return custom payload. **/
   if (distanceData.length > 0) {
     /** format custom payload for line **/
-    const columns: LineColumns[] = distanceData.map((distance: any) => {
+    const contents = distanceData.map((distance: any) => {
       return {
-        thumbnailImageUrl: distance.image,
-        imageBackgroundColor: "#FFFFFF",
-        title: distance.name.replace(/(.{40})..+/, "$1…"),
-        text: distance.distance,
-        actions: [
-          {
-            type: "uri",
-            label: "เปิดแผนที่",
-            uri: `http://maps.google.com/maps?z=12&t=m&q=loc:${distance.latitude}+${distance.longitude}`,
-          },
-        ],
+        type: "bubble",
+        size: "micro",
+        hero: {
+          type: "image",
+          url: distance.image,
+          size: "full",
+          aspectMode: "cover",
+          aspectRatio: "320:213",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: distance.name.replace(/(.{40})..+/, "$1…"),
+              weight: "bold",
+              size: "sm",
+              wrap: true,
+            },
+            {
+              type: "box",
+              layout: "baseline",
+              contents: [
+                {
+                  type: "icon",
+                  size: "xs",
+                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                },
+                {
+                  type: "icon",
+                  size: "xs",
+                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                },
+                {
+                  type: "icon",
+                  size: "xs",
+                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                },
+                {
+                  type: "icon",
+                  size: "xs",
+                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                },
+                {
+                  type: "icon",
+                  size: "xs",
+                  url: "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                },
+                {
+                  type: "text",
+                  text: "4.0",
+                  size: "xs",
+                  color: "#8c8c8c",
+                  margin: "md",
+                },
+              ],
+            },
+            {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "box",
+                  layout: "baseline",
+                  spacing: "sm",
+                  contents: [
+                    {
+                      type: "text",
+                      text: distance.distance,
+                      wrap: true,
+                      color: "#8c8c8c",
+                      size: "xs",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+          spacing: "sm",
+          paddingAll: "13px",
+        },
+        footer: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "separator",
+            },
+            {
+              type: "button",
+              action: {
+                type: "uri",
+                label: "เปิดแผนที่",
+                uri: `http://maps.google.com/maps?z=12&t=m&q=loc:${distance.latitude}+${distance.longitude}`,
+              },
+            },
+          ],
+        },
       };
     });
 
-    const payload: Line = {
+    const payload = {
       line: {
-        type: "template",
-        altText: "this is a carousel template",
-        template: {
+        type: "flex",
+        altText: "Flex Message",
+        contents: {
           type: "carousel",
-          imageAspectRatio: "rectangle",
-          imageSize: "cover",
-          columns: columns,
+          contents: contents,
         },
       },
     };
