38948c07a60de4ab7e9e61a57d6ed0e0fb275d91
tapptm <phuwadet00123@gmail.com>
2023-01-18 12:37:09 +0700

    [fix]: line response payloads

--- dist/index.js
+++ dist/index.js
@@ -9,10 +9,12 @@ const handleGreeting_1 = require("./src/handles/handleGreeting");
 const handlePointOfInterest_1 = require("./src/handles/handlePointOfInterest");
 const dotenv_1 = __importDefault(require("dotenv"));
 const actions_on_google_1 = require("actions-on-google");
+const https_1 = __importDefault(require("https"));
 const dfl = (0, actions_on_google_1.dialogflow)();
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.NODE_PORT || 4050;
+const TOKEN = process.env.LINE_ACCESS_TOKEN;
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Server Is Working......");
@@ -22,10 +24,9 @@ app.get("/", (req, res) => {
  * For the dialogflow we need POST Route.
  **/
 app.post("/webhook", (req, res) => {
-    console.log(req);
+    console.log(req.body);
     // get agent from request
     let agent = new dialogflow_fulfillment_1.WebhookClient({ request: req, response: res });
-    console.log(agent);
     // create intentMap for handle intent
     // dfl.intent("webhook", conv => {
     //   conv.ask(
@@ -53,6 +54,52 @@ app.post("/webhook", (req, res) => {
     intentMap.set("ร้านอาหาร", handleGreeting_1.getGreeting);
     // now agent is handle request and pass intent map
     agent.handleRequest(intentMap);
+});
+app.post("/webhooks", function (req, res) {
+    res.send("HTTP POST request sent to the webhook URL!");
+    // If the user sends a message to your bot, send a reply message
+    if (req.body.events[0].type === "message") {
+        // Message data, must be stringified
+        const dataString = JSON.stringify({
+            replyToken: req.body.events[0].replyToken,
+            messages: [
+                {
+                    "type": "text",
+                    "text": "Hello, user"
+                },
+                {
+                    "type": "text",
+                    "text": "May I help you?"
+                }
+            ]
+        });
+        // Request header
+        const headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + TOKEN
+        };
+        // Options to pass into the request
+        const webhookOptions = {
+            "hostname": "api.line.me",
+            "path": "/v2/bot/message/reply",
+            "method": "POST",
+            "headers": headers,
+            "body": dataString
+        };
+        // Define request
+        const request = https_1.default.request(webhookOptions, (res) => {
+            res.on("data", (d) => {
+                process.stdout.write(d);
+            });
+        });
+        // Handle error
+        request.on("error", (err) => {
+            console.error(err);
+        });
+        // Send data
+        request.write(dataString);
+        request.end();
+    }
 });
 app.listen(port, () => {
     console.log(`Server is running at port: ${port}`);
--- index.ts
+++ index.ts
@@ -4,11 +4,14 @@ import { getGreeting } from "./src/handles/handleGreeting";
 import { getlocation } from "./src/handles/handlePointOfInterest";
 import dotenv from "dotenv";
 import { dialogflow, Permission, SimpleResponse } from "actions-on-google";
+import request from "request-promise";
+import https from "https";
 
-const dfl = dialogflow()
+const dfl = dialogflow();
 dotenv.config();
 const app: Express = express();
 const port = process.env.NODE_PORT || 4050;
+const TOKEN = process.env.LINE_ACCESS_TOKEN
 
 app.use(express.json());
 
@@ -20,12 +23,10 @@ app.get("/", (req: Request, res: Response) => {
  * For the dialogflow we need POST Route.
  **/
 app.post("/webhook", (req: Request, res: Response) => {
-  console.log(req);
-  
+  console.log(req.body);
+
   // get agent from request
   let agent = new WebhookClient({ request: req, response: res });
-  console.log(agent);
-  
   // create intentMap for handle intent
   // dfl.intent("webhook", conv => {
   //   conv.ask(
@@ -47,7 +48,6 @@ app.post("/webhook", (req: Request, res: Response) => {
     );
   });
 
-  
   // intent poi
   intentMap.set("ธนาคาร", getlocation);
   intentMap.set("โรงพยาบาล", getlocation);
@@ -55,10 +55,62 @@ app.post("/webhook", (req: Request, res: Response) => {
   intentMap.set("ปั้มน้ำมัน", getlocation);
   intentMap.set("ธนาคาร", getlocation);
   intentMap.set("ร้านอาหาร", getGreeting);
-  
+
   // now agent is handle request and pass intent map
   agent.handleRequest(intentMap);
 });
+
+app.post("/webhooks", function(req: Request, res: Response) {
+  res.send("HTTP POST request sent to the webhook URL!")
+  // If the user sends a message to your bot, send a reply message
+  if (req.body.events[0].type === "message") {
+    // Message data, must be stringified
+    const dataString = JSON.stringify({
+      replyToken: req.body.events[0].replyToken,
+      messages: [
+        {
+          "type": "text",
+          "text": "Hello, user"
+        },
+        {
+          "type": "text",
+          "text": "May I help you?"
+        }
+      ]
+    })
+
+    // Request header
+    const headers = {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + TOKEN
+    }
+
+    // Options to pass into the request
+    const webhookOptions = {
+      "hostname": "api.line.me",
+      "path": "/v2/bot/message/reply",
+      "method": "POST",
+      "headers": headers,
+      "body": dataString
+    }
+
+    // Define request
+    const request = https.request(webhookOptions, (res) => {
+      res.on("data", (d) => {
+        process.stdout.write(d)
+      })
+    })
+
+    // Handle error
+    request.on("error", (err) => {
+      console.error(err)
+    })
+
+    // Send data
+    request.write(dataString)
+    request.end()
+  }
+})
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
