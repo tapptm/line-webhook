5c5a88e90ebb336716cc32552061e3740b6edb25
tapptm <phuwadet00123@gmail.com>
2023-01-21 15:59:29 +0700

    [fix]: refactor code.

--- index.ts
+++ index.ts
@@ -1,17 +1,10 @@
 import express, { Express, Request, Response } from "express";
-import { getlocationPointOfInterest } from "./src/handles/handlePointOfInterest";
-import { getlocationRestaurants } from "./src/handles/handleRestaurant";
-import { getlocationActivitys } from "./src/handles/handleActivity";
-import { getlocationHotels } from "./src/handles/handleHotel";
-import { sessionClient, sessionPath } from "./src/configs/dialogflow";
-import { replyMessage } from "./src/services/linesdk/linesdkService";
-import { postToDialogflow } from "./src/services/dialogflows/dialogflowService";
-import { saveChats, getChats } from "./src/models/chatHistorys";
+import { webhooksController } from "./src/controllers/webhooks";
 import dotenv from "dotenv";
-
 dotenv.config();
+
 const app: Express = express();
-const port = process.env.NODE_PORT || 4050;
+const PORT = process.env.NODE_PORT || 4050;
 
 app.use(express.json());
 
@@ -19,103 +12,8 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Server Is Working......");
 });
 
-app.post("/webhooks", async function (req: Request, res: Response) {
-  const event = req.body.events[0];
-  console.log(req.body.events);
+app.post("/webhooks", webhooksController);
 
-  if (event.type === "message" && event.message.type === "text") {
-    try {
-      await postToDialogflow(req);
-      console.log("TEST OK");
-      const requestIntent = {
-        session: sessionPath,
-        queryInput: {
-          text: {
-            text: event.message.text,
-            languageCode: "th-TH",
-          },
-        },
-      };
-      const responses = await sessionClient.detectIntent(requestIntent);
-      const result: any = responses[0].queryResult;
-      const intent = result.intent.displayName;
-
-      if (
-        intent === "โรงพยาบาล" ||
-        intent === "ร้านค้า" ||
-        intent === "ปั้มน้ำมัน" ||
-        intent === "ธนาคาร" ||
-        intent === "ตลาด" ||
-        intent === "ร้านกาแฟ" ||
-        intent === "ร้านซ่อมรถ" ||
-        intent === "ร้านถ่ายรูป" ||
-        intent === "วัด" ||
-        intent === "ร้านอาหาร" ||
-        intent === "ศาลเจ้าพ่อ" ||
-        intent === "สถานีตำรวจ" ||
-        intent === "สถานีรถไฟ" ||
-        intent === "ที่พัก" ||
-        intent === "กิจกรรม"
-      ) {
-        await saveChats(
-          event.source.userId,
-          result.intent.displayName,
-          event.message.text
-        );
-      }
-    } catch (error: any) {
-      res.send({ message: error.message });
-    }
-  } else if (event.type === "message" && event.message.type === "location") {
-    try {
-      const chats = await getChats(event.source.userId);
-      let lastChat = chats[chats.length - 1];
-      console.log("LAST_CHAT", lastChat);
-      
-      if (lastChat.intent_name === "ร้านอาหาร") {
-        console.log("FOOD ON");
-        return await getlocationRestaurants({
-          intent: lastChat.intent_name,
-          latitude: event.message.latitude,
-          longitude: event.message.longitude,
-          userId: event.source.userId,
-        });
-      } else if (lastChat.intent_name === "ที่พัก") {
-        console.log("HOTEL ON");
-        await getlocationHotels({
-          intent: lastChat.intent_name,
-          latitude: event.message.latitude,
-          longitude: event.message.longitude,
-          userId: event.source.userId,
-        });
-      } else if (lastChat.intent_name === "กิจกรรม") {
-        console.log("ACTIVITY ON");
-        await getlocationActivitys({
-          intent: lastChat.intent_name,
-          latitude: event.message.latitude,
-          longitude: event.message.longitude,
-          userId: event.source.userId,
-        });
-      } else {
-        console.log("POI ON");
-        await getlocationPointOfInterest({
-          intent: lastChat.intent_name,
-          latitude: event.message.latitude,
-          longitude: event.message.longitude,
-          userId: event.source.userId,
-        });
-      }
-    } catch (error: any) {
-      res.send({ message: error.message });
-    }
-  } else {
-    replyMessage(
-      event.source.userId,
-      `Sorry, this chatbot did not support message type ${event.message.type}`
-    );
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at port: ${PORT}`);
 });
---
+++ src/controllers/webhooks.ts
@@ -0,0 +1,108 @@
+import { Request, Response } from "express";
+import { getlocationPointOfInterest } from "../handles/handlePointOfInterest";
+import { getlocationRestaurants } from "../handles/handleRestaurant";
+import { getlocationActivitys } from "../handles/handleActivity";
+import { getlocationHotels } from "../handles/handleHotel";
+import { sessionClient, sessionPath } from "../configs/dialogflow";
+import { replyMessage } from "../services/linesdk/linesdkService";
+import { postToDialogflow } from "../services/dialogflows/dialogflowService";
+import { saveChats, getChats } from "../models/chatHistorys";
+
+async function webhooksController(req: Request, res: Response) {
+  const event = req.body.events[0];
+  console.log(req.body.events);
+
+  if (event.type === "message" && event.message.type === "text") {
+    try {
+      await postToDialogflow(req);
+      console.log("TEST OK");
+      const requestIntent = {
+        session: sessionPath,
+        queryInput: {
+          text: {
+            text: event.message.text,
+            languageCode: "th-TH",
+          },
+        },
+      };
+      const responses = await sessionClient.detectIntent(requestIntent);
+      const result: any = responses[0].queryResult;
+      const intent = result.intent.displayName;
+
+      if (
+        intent === "โรงพยาบาล" ||
+        intent === "ร้านค้า" ||
+        intent === "ปั้มน้ำมัน" ||
+        intent === "ธนาคาร" ||
+        intent === "ตลาด" ||
+        intent === "ร้านกาแฟ" ||
+        intent === "ร้านซ่อมรถ" ||
+        intent === "ร้านถ่ายรูป" ||
+        intent === "วัด" ||
+        intent === "ร้านอาหาร" ||
+        intent === "ศาลเจ้าพ่อ" ||
+        intent === "สถานีตำรวจ" ||
+        intent === "สถานีรถไฟ" ||
+        intent === "ที่พัก" ||
+        intent === "กิจกรรม"
+      ) {
+        await saveChats(
+          event.source.userId,
+          result.intent.displayName,
+          event.message.text
+        );
+      }
+    } catch (error: any) {
+      res.send({ message: error.message });
+    }
+  } else if (event.type === "message" && event.message.type === "location") {
+    try {
+      const chats = await getChats(event.source.userId);
+      let lastChat = chats[chats.length - 1];
+      console.log("LAST_CHAT", lastChat);
+
+      if (lastChat.intent_name === "ร้านอาหาร") {
+        console.log("FOOD ON");
+        return await getlocationRestaurants({
+          intent: lastChat.intent_name,
+          latitude: event.message.latitude,
+          longitude: event.message.longitude,
+          userId: event.source.userId,
+        });
+      } else if (lastChat.intent_name === "ที่พัก") {
+        console.log("HOTEL ON");
+        await getlocationHotels({
+          intent: lastChat.intent_name,
+          latitude: event.message.latitude,
+          longitude: event.message.longitude,
+          userId: event.source.userId,
+        });
+      } else if (lastChat.intent_name === "กิจกรรม") {
+        console.log("ACTIVITY ON");
+        await getlocationActivitys({
+          intent: lastChat.intent_name,
+          latitude: event.message.latitude,
+          longitude: event.message.longitude,
+          userId: event.source.userId,
+        });
+      } else {
+        console.log("POI ON");
+        await getlocationPointOfInterest({
+          intent: lastChat.intent_name,
+          latitude: event.message.latitude,
+          longitude: event.message.longitude,
+          userId: event.source.userId,
+        });
+      }
+    } catch (error: any) {
+      res.send({ message: error.message });
+    }
+  } else {
+    replyMessage(
+      event.source.userId,
+      `Sorry, this chatbot did not support message type ${event.message.type}`
+    );
+  }
+}
+
+export { webhooksController };
